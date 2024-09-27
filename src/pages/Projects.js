e0e1d6444ab0a5b2b67c48320ff7e55bdf68e96c
ftischle <frederic.tischler@protonmail.com>
2024-09-27 20:59:14 +0200

    image projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './Projects.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import turingImage from '../img/turing.webp';
+import groupieImage from '../img/groupie-tracker.png'
+import lemImage from '../img/lem-in.webp'
+import wordsImage from '../img/words-shared.webp'
+import graphImage from '../img/graph.webp'
+import systemImage from '../img/client-server.webp'
+
 
 const Projects = () => {
     const projectList = [
@@ -9,42 +16,45 @@ const Projects = () => {
             name: 'Turing Machine',
             description: 'An interactive simulation of a Turing machine with a graphical java swing interface, using MVC design pattern.',
             technologies: ['Java'],
-            githubLink: 'https://github.com/FredericTischler/Turing-Machine'
+            githubLink: 'https://github.com/FredericTischler/Turing-Machine',
+            imageUrl: turingImage,
         },
         {
             name: 'Groupie Tracker',
             description: 'A website utilizing an API to display information about artists and their concerts.',
             technologies: ['API', 'JavaScript', 'HTML/CSS', 'Go'],
-            githubLink: 'https://github.com/FredericTischler/Groupie-tracker'
+            githubLink: 'https://github.com/FredericTischler/Groupie-tracker',
+            imageUrl: groupieImage,
         },
         {
             name: 'Lem-in',
             description: 'A pathfinding algorithm where ants must find the shortest route through a network of tunnels while following movement constraints.',
             technologies: ['Go'],
-            githubLink: 'https://github.com/FredericTischler/Lem-in'
+            githubLink: 'https://github.com/FredericTischler/Lem-in',
+            imageUrl: lemImage
         },
         {
             name: 'Words Shared',
             description: 'A C program that lists and displays words shared between multiple files, sorted by occurrence in descending order and lexicographically.',
             technologies: ['C'],
-            githubLink: 'https://github.com/FredericTischler/Words-Shared'
+            githubLink: 'https://github.com/FredericTischler/Words-Shared',
+            imageUrl: wordsImage,
         },
         {
             name: 'Graph',
             description: 'A Python project analyzing and testing several algorithms related to graph theory.',
             technologies: ['Python'],
-            githubLink: 'https://github.com/FredericTischler/Projet-Graphe'
+            githubLink: 'https://github.com/FredericTischler/Projet-Graphe',
+            imageUrl: graphImage,
         },
         {
             name: 'System Client/Server',
             description: 'A client-server system where the client sends bash commands to the server, which executes them on dedicated threads and returns the result.',
             technologies: ['C'],
-            githubLink: 'https://github.com/FredericTischler/Server-client'
+            githubLink: 'https://github.com/FredericTischler/Server-client',
+            imageUrl: systemImage,
         }
     ];
-
-
-
 
     return (
         <section id="projects" className="projects-section">
@@ -52,16 +62,19 @@ const Projects = () => {
             <div className="projects-container">
                 {projectList.map((project, index) => (
                     <div key={index} className="project-card">
-                        <h3>{project.name}</h3>
-                        <p>{project.description}</p>
-                        <div className="technologies">
-                            {project.technologies.map((tech, techIndex) => (
-                                <span key={techIndex} className="tech-badge">{tech}</span>
-                            ))}
+                        <img src={project.imageUrl} alt={project.name} className="project-image" /> {/* Image on the left */}
+                        <div className="project-content"> {/* Text on the right */}
+                            <h3>{project.name}</h3>
+                            <p>{project.description}</p>
+                            <div className="technologies">
+                                {project.technologies.map((tech, techIndex) => (
+                                    <span key={techIndex} className="tech-badge">{tech}</span>
+                                ))}
+                            </div>
+                            <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="github-link">
+                                <FontAwesomeIcon icon={faGithub} /> View on GitHub
+                            </a>
                         </div>
-                        <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="github-link">
-                            <FontAwesomeIcon icon={faGithub} /> View on GitHub
-                        </a>
                     </div>
                 ))}
             </div>
