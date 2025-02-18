1f1f6be7a40da09ab1053ebd41008e8b6d9bfe03
ftischle <frederic.tischler@protonmail.com>
2025-02-18 13:07:25 +0100

    Cv download

--- src/pages/aboutme/AboutMe.js
+++ src/pages/aboutme/AboutMe.js
@@ -40,6 +40,11 @@ const AboutMe = () => {
                     <p>
                         Passionate about AI, I aim to develop a model for financial market prediction. Currently seeking an apprenticeship to further enhance my skills.
                     </p>
+                    <p>
+                        <a href="../../img/CV_Frederic_Tischler.pdf" download="FredericTischler_CV.pdf" className="cv-link">
+                            <button className="download-cv-button">Download My CV</button>
+                        </a>
+                    </p>
                 </div>
             </div>
             <div style={{ height: '300px', width: '97%', position: 'absolute' }}>
