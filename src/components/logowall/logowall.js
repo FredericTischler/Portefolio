3c8dec9d9e9173c4428468a4c38c36ed74d51d67
ftischle <frederic.tischler@protonmail.com>
2025-02-19 12:57:12 +0100

    icones upgrade

--- src/components/logowall/logowall.js
+++ src/components/logowall/logowall.js
@@ -2,62 +2,64 @@ import { useState } from "react";
 import "./LogoWall.css";
 
 function LogoWall({
-  items = [],
-  direction = "horizontal",
-  pauseOnHover = false,
-  size = "clamp(8rem, 1rem + 30vmin, 25rem)",
-  duration = "60s",
-  textColor = "#ffffff",
-  bgColor = "#060606",
-  bgAccentColor = "#111111"
-}) {
+                    items = [],
+                    direction = "horizontal",
+                    pauseOnHover = false,
+                    size = "clamp(8rem, 1rem + 30vmin, 25rem)",
+                    duration = "60s",
+                    textColor = "#ffffff",
+                    bgColor = "#060606",
+                    bgAccentColor = "#111111"
+                  }) {
   const [isPaused, setIsPaused] = useState(false);
 
   const wrapperClass = [
     "logoWall-wrapper",
     direction === "vertical" && "wrapper--vertical"
   ]
-    .filter(Boolean)
-    .join(" ");
+      .filter(Boolean)
+      .join(" ");
 
   const marqueeClass = [
     "marquee",
     direction === "vertical" && "marquee--vertical",
     isPaused && "paused"
   ]
-    .filter(Boolean)
-    .join(" ");
+      .filter(Boolean)
+      .join(" ");
 
   return (
-    <article
-      className={wrapperClass}
-      style={{
-        "--size": size,
-        "--duration": duration,
-        "--color-text": textColor,
-        "--color-bg": bgColor,
-        "--color-bg-accent": bgAccentColor
-      }}
-    >
-      <div
-        className={marqueeClass}
-        onMouseEnter={() => pauseOnHover && setIsPaused(true)}
-        onMouseLeave={() => pauseOnHover && setIsPaused(false)}
+      <article
+          className={wrapperClass}
+          style={{
+            "--size": size,
+            "--duration": duration,
+            "--color-text": textColor,
+            "--color-bg": bgColor,
+            "--color-bg-accent": bgAccentColor
+          }}
       >
-        <div className="marquee__group">
-          {items.map((item, idx) => (
-            <img key={idx} src={item.imgUrl} alt={item.altText} />
-          ))}
+        <div
+            className={marqueeClass}
+            onMouseEnter={() => pauseOnHover && setIsPaused(true)}
+            onMouseLeave={() => pauseOnHover && setIsPaused(false)}
+        >
+          <div className="marquee__group">
+            {items.map((item, idx) => (
+                <a href={item.link} target="_blank" rel="noopener noreferrer" key={idx}>
+                  <img src={item.imgUrl} alt={item.altText} />
+                </a>
+            ))}
+          </div>
+          <div className="marquee__group" aria-hidden="true">
+            {items.map((item, idx) => (
+                <a href={item.link} target="_blank" rel="noopener noreferrer" key={`dup1-${idx}`}>
+                  <img src={item.imgUrl} alt={item.altText} />
+                </a>
+            ))}
+          </div>
         </div>
-        <div className="marquee__group" aria-hidden="true">
-          {items.map((item, idx) => (
-            <img key={`dup1-${idx}`} src={item.imgUrl} alt={item.altText} />
-          ))}
-        </div>
-      </div>
-
-     
-    </article>
+      </article>
   );
 }
 
--- src/pages/aboutme/AboutMe.js
+++ src/pages/aboutme/AboutMe.js
@@ -11,24 +11,44 @@ import Css3Icon from '../../icons/css3.svg';
 import MySqlIcon from '../../icons/mysql.svg';
 import sqliteIcon from '../../icons/sqlite.svg';
 import cIcon from '../../icons/c.svg';
-import javaIcon from '../../icons/java.svg'
+import javaIcon from '../../icons/java.svg';
+import springIcon from '../../icons/spring.svg';
+import phpIcon from '../../icons/php.svg';
+import nodeIcon from '../../icons/nodedotjs.svg';
+import tsIcon from '../../icons/typescript.svg';
+import bootsIcon from '../../icons/bootstrap.svg';
+import tailIcon from '../../icons/tailwindcss.svg';
+import dockerIcon from '../../icons/docker.svg';
+import kuberIcon from '../../icons/kubernetes.svg';
+import gitIcon from '../../icons/git.svg';
+import jetIcon from '../../icons/jetbrains.svg';
 import LogoWall from '../../components/logowall/logowall';
 
 const AboutMe = () => {
     const logoImgs = [
-        { imgUrl: JsIcon, altText: "JavaScript" },
-        { imgUrl: AngularIcon, altText: "React Bits Logo" },
-        { imgUrl: ReactIcon, altText: "React Bits Logo" },
-        { imgUrl: PythonIcon, altText: "React Bits Logo" },
-        { imgUrl: GolangIcon, altText: "React Bits Logo" },
-        { imgUrl: RustIcon, altText: "React Bits Logo" },
-        { imgUrl: Html5Icon, altText: "React Bits Logo" },
-        { imgUrl: Css3Icon, altText: "React Bits Logo" },
-        { imgUrl: MySqlIcon, altText: "React Bits Logo" },
-        { imgUrl: sqliteIcon, altText: "React Bits Logo" },
-        { imgUrl: javaIcon, altText: "React Bits Logo" },
-        { imgUrl: cIcon, altText: "React Bits Logo" }
-      ];
+        { imgUrl: JsIcon, altText: "JavaScript", link: "https://www.javascript.com/" },
+        { imgUrl: tsIcon, altText: "TypeScript", link: "https://www.typescriptlang.org/" },
+        { imgUrl: PythonIcon, altText: "Python", link: "https://www.python.org/" },
+        { imgUrl: GolangIcon, altText: "Golang", link: "https://golang.org/" },
+        { imgUrl: RustIcon, altText: "Rust", link: "https://www.rust-lang.org/" },
+        { imgUrl: javaIcon, altText: "Java", link: "https://www.java.com/" },
+        { imgUrl: cIcon, altText: "C", link: "https://en.wikipedia.org/wiki/C_(programming_language)" },
+        { imgUrl: phpIcon, altText: "PHP", link: "https://www.php.net/" },
+        { imgUrl: AngularIcon, altText: "Angular", link: "https://angular.io/" },
+        { imgUrl: ReactIcon, altText: "React", link: "https://reactjs.org/" },
+        { imgUrl: nodeIcon, altText: "Node.js", link: "https://nodejs.org/" },
+        { imgUrl: springIcon, altText: "Spring", link: "https://spring.io/" },
+        { imgUrl: MySqlIcon, altText: "MySQL", link: "https://www.mysql.com/" },
+        { imgUrl: sqliteIcon, altText: "SQLite", link: "https://www.sqlite.org/" },
+        { imgUrl: Html5Icon, altText: "HTML", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+        { imgUrl: Css3Icon, altText: "CSS", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+        { imgUrl: bootsIcon, altText: "Bootstrap", link: "https://getbootstrap.com/" },
+        { imgUrl: tailIcon, altText: "Tailwind CSS", link: "https://tailwindcss.com/" },
+        { imgUrl: dockerIcon, altText: "Docker", link: "https://www.docker.com/" },
+        { imgUrl: kuberIcon, altText: "Kubernetes", link: "https://kubernetes.io/" },
+        { imgUrl: gitIcon, altText: "Git", link: "https://git-scm.com/" },
+        { imgUrl: jetIcon, altText: "JetBrains", link: "https://www.jetbrains.com/" }
+    ];
     return (
         <section id="about-me" className="about-me-section">
             <h2 className="section-title">About Me</h2>
@@ -54,7 +74,7 @@ const AboutMe = () => {
                             duration='30s'
                             bgColor='transparent'
                             bgAccentColor='transparent'
-                        />  
+                        />
             </div>
         </section>
     );
